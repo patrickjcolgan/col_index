49df1a918cb8e04b1ed3f241e915f81b7738d313
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-05-19 10:22:19 -0700

    obj constructor function City

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//Perhaps our City instances will be stored here?
+var allCities [];
+
+//Object Constructor function to store city data
+function City(city, state, income, longitude, latitude) {//do we need parameters??
+  this.city = city;
+  this.state = state;
+  this.income = income;
+  this.longitude = longitude;
+  this.latitude = latitude;
+  //insert subcategories here
+};
