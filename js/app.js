04143c2d108585e4ceb75f989aba577de4cd87c2
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-05-19 19:29:48 -0700

    add subcategories and google map link

--- js/app.js
+++ js/app.js
@@ -8,5 +8,13 @@ function City(city, state, income, longitude, latitude) {//do we need parameters
   this.income = income;
   this.longitude = longitude;
   this.latitude = latitude;
-  //insert subcategories here
+  this.grocery = grocery;
+  this.housing = housing;
+  this.utilities = utilities;
+  this.transportation = transportation;
+  this.healthcare = healthcare;
+  this.miscellaneous = miscellaneous;
 };
+
+//Google Maps
+var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
