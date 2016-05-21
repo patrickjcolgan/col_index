8c71be4c2f1467013672651d36791d76ed08662d
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-05-21 14:43:30 -0700

    storing userInput number value

--- js/results.js
+++ js/results.js
@@ -1,12 +1,21 @@
-var resultArray [];
-var getIncome = document.getElementById('income');
-var lowerIncomeRange = getIncome - 5000;
-var higherIncomeRange = getIncome + 5000;
+//event listener
+// var getIncome = document.getElementById('income');
+document.getElementById('indexSearch').addEventListener('submit', function(event){
+  event.preventDefault();
+  var userInput = event.target.income.value;
+  console.log(userInput);
+  console.log("hey");
+});
 
-for (var i in allMetro) {
-  if (lowerIncomeRange < allMetro.income[i] && allMetro.income[i] < higherIncomeRange) {
-    var metroResult = allMetro[i].name;
-    metroResult.push(allMetro[i].name);
-    push.resultArray;
-  }
-}
+// var resultArray = [];
+// // getIncome = parseInt(getIncome);
+// var lowerIncomeRange = getIncome - 5000;
+// var higherIncomeRange = getIncome + 5000;
+//
+// for (var i in allMetro) {
+//   if (lowerIncomeRange < allMetro.income[i] && allMetro.income[i] < higherIncomeRange) {
+//     var metroResult = allMetro[i].name;
+//     metroResult.push(allMetro[i].name);
+//     push.resultArray;
+//   }
+// }
