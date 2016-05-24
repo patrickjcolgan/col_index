4318ba7bf275aeeb34734ff1f55cca50b81bc323
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-05-23 21:06:23 -0700

    results array complete

--- js/results.js
+++ js/results.js
@@ -1,52 +1,60 @@
+var dupeState;
+var resultsArr = [];
+
 //event listener
-document.getElementById('indexSearch').addEventListener('submit', function(event){
+document.getElementById('indexSearch').addEventListener('submit', function(event) {
   event.preventDefault();
   userInput = event.target.income.value;
-  if (userInput >= 40000){
+  if (userInput >= 40000) {
     // console.log(userInput);
     var locationInput = event.target.location.value;
     // console.log(locationInput);
-    matchInput(5000);
+    matchInput(1000);
   } else {
     alert('Please enter at least $40,000');
   }
+  if (resultsArr.length < 10) {
+    console.log('2500');
+    console.log(dupeState);
+    matchInput(5000);
+  }
+  if (resultsArr.length < 10) {
+    console.log('5000');
+    console.log(dupeState);
+    matchInput(10000);
+  }
+  if (resultsArr.length < 10) {
+    console.log('10000');
+    console.log(dupeState);
+    matchInput(20000);
+  }
 });
 
-var resultsArr = [];
-
 //checks for duplications in resultsArr
-function checkDupArray (array1, array2){
-  if (array1.indexOf(array2) === -1 && array1.length < 5){
-    array1.push(array2);
+function checkDupArray(array1, array2) {
+  if (array1.indexOf(array2) === -1) {
     console.log('new array is ' + array1);
-    if (array1.length < 5){
-      matchInput(10000);
-      console.log('10k');
-    } if (array1.length < 5){
-      matchInput(15000);
-      console.log('15k');
-    } if (array1.length < 5){
-      matchInput(20000);
-      console.log('20k');
-    }
-  } else if (array1.indexOf(array2) > -1){
+    dupeState = true;
+  } else if (array1.indexOf(array2) > -1) {
     console.log(array2 + 'already exists in the array');
+    dupeState = false;
   }
 };
 
 //matches userInput with suggested cities based on cost of living
-matchInput = function(range){
+matchInput = function(range) {
   userInput = parseInt(userInput);
   var lowerIncomeRange = userInput - parseInt(range);
   var higherIncomeRange = userInput + parseInt(range);
-  // console.log(lowerIncomeRange);
-  // console.log(higherIncomeRange);
-
-    for (var i = 0; i < allMetro.length; i++){
-      if (lowerIncomeRange < allMetro[i].income && allMetro[i].income < higherIncomeRange){
-        checkDupArray(resultsArr, allMetro[i].name);
+  for (var i = 0; i < allMetro.length; i++) {
+    if (lowerIncomeRange < allMetro[i].income && allMetro[i].income < higherIncomeRange) {
+      checkDupArray(resultsArr, allMetro[i].name);
+      console.log('dupestate succssfully processed');
+      if (dupeState === true) {
+        resultsArr.push(allMetro[i].name);
+        console.log(dupeState);
+        console.log(resultsArr.length);
       }
     }
-  };
-
-  //Get data for current location to render in bar chart
+  }
+};
